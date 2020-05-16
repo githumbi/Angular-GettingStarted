4db8bc63a9841a82838e6415ea1c13a31909d791
githumbi <githumbi74@gmail.com>
2020-05-16 18:28:00 +0300

    put products thru dependency injection

--- Recap/src/app/app.module.ts
+++ Recap/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpace } from './shared/convert-to-space.pipe';
+
 @NgModule({
   declarations: [AppComponent, ProductListComponent, ConvertToSpace],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
