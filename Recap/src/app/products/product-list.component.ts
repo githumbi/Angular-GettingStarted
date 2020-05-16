5ecc57995e57c5c7fd39eb5ea07996135c04e64e
githumbi <githumbi74@gmail.com>
2020-05-16 17:25:14 +0300

    added ngmodel

--- Recap/src/app/products/product-list.component.ts
+++ Recap/src/app/products/product-list.component.ts
@@ -7,6 +7,7 @@ import { Iproducts } from './products';
 })
 export class ProductListComponent {
   showImage = false;
+  filterProduct = 'sample';
   productList: Iproducts[] = [
     {
       productId: 1,
